bd1b2340425c07c369d7db648f696ec15d5bba00
Vicente Cubells <vcubells@yahoo.com>
2015-08-20 15:59:31 -0500

    Se adicionaron los ejemplos de apuntadores a funciones

---
+++ Tema_1_Apuntadores/menu_opciones/menu_opciones/main.c
@@ -0,0 +1,89 @@
+//
+//  main.c
+//  menu_opciones
+//
+//  Created by Vicente Cubells Nonell on 20/08/15.
+//  Copyright (c) 2015 Vicente Cubells Nonell. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 4
+
+typedef void (*opcion_t) (int);
+
+void abrir(int);
+void guardar(int);
+void salir(int);
+void copiar(int);
+
+int main(int argc, const char * argv[]) {
+    
+    opcion_t * menu = (opcion_t *) malloc( N * sizeof(opcion_t));
+    
+    *menu = abrir;
+    *(menu+1) = guardar;
+    *(menu+2) = salir;
+    *(menu+3) = copiar;
+    
+    
+    printf("--- Opciones --- \n1-Abrir\n2-Guardar\n3-Salir\n4-Copiar\nEscoge tu opción: ");
+    
+    int opcion;
+    
+    scanf("%d", &opcion);
+    
+    /* Utilizando switch */
+    
+//    switch (opcion) {
+//        case 1:
+//            abrir(opcion);
+//            break;
+//        
+//        case 2:
+//            guardar(opcion);
+//            break;
+//            
+//        case 3:
+//            salir(opcion);
+//            break;
+//            
+//        case 4:
+//            copiar(opcion);
+//            break;
+//            
+//        default:
+//            printf("Opción no válida\n");
+//            break;
+//    }
+    
+    /* Apuntadores a funciones */
+    
+    (*(menu+opcion-1))(opcion);
+    
+    free(menu);
+    
+    return 0;
+}
+
+void abrir(int opcion)
+{
+    printf("%d - Abriendo el archivo...\n", opcion);
+}
+
+void guardar(int opcion)
+{
+    printf("%d - Guardando el archivo...\n", opcion);
+}
+
+void salir(int opcion)
+{
+    printf("%d - Terminando el programa...\n", opcion);
+}
+
+void copiar(int opcion)
+{
+    printf("%d - Copiando el archivo...\n", opcion);
+}
+
---
+++ Tema_1_Apuntadores/ordenamiento/ordenamiento/main.c
@@ -0,0 +1,71 @@
+//
+//  main.c
+//  ordenamiento
+//
+//  Created by Vicente Cubells Nonell on 20/08/15.
+//  Copyright (c) 2015 Vicente Cubells Nonell. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+
+void ordena(int *, int (*)(int, int));
+int asc(int, int);
+int desc(int, int);
+
+int main(int argc, const char * argv[]) {
+   
+    int * valores = (int *) malloc(N * sizeof(int));
+    
+    int * p = valores;
+    
+    srand((int) time(NULL));
+    
+    for (; p < (valores + N); ++p) {
+        *p = rand() % 100;
+        printf("%4d", *p);
+    }
+    
+    ordena(valores, desc);
+    
+    
+    printf("\n\n--- Ordenados ---\n\n");
+    
+    for (p = valores; p < (valores + N); ++p) {
+        printf("%4d", *p);
+    }
+    
+    free(valores);
+    
+    return 0;
+}
+
+void ordena(int * numeros, int criterio(int, int))
+{
+    int i, j, temp;
+    
+    for(i = 0; i < N; ++i)
+    {
+        for(j = N-1; j > i; --j)
+        {
+            if (criterio(numeros[j],numeros[j-1])) {
+                temp = numeros[j-1];
+                numeros[j-1] = numeros[j];
+                numeros[j] = temp;
+            }
+        }
+    }
+}
+
+int asc(int a, int b)
+{
+    return a < b;
+}
+
+int desc(int a, int b)
+{
+    return b < a;
+}
