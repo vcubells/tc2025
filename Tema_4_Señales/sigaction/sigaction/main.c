e2b0a1a1595dc744aa2e7ad9155ac56952778689
Vicente Cubells <vcubells@yahoo.com>
2015-10-08 15:12:49 -0500

    ejemplo sigaction

---
+++ Tema_4_Señales/sigaction/sigaction/main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  sigaction
+//
+//  Created by Vicente Cubells Nonell on 08/10/15.
+//  Copyright © 2015 Vicente Cubells Nonell. All rights reserved.
+//
+
+#include <stdio.h>
+#include <signal.h>
+
+void gestor(int s)
+{
+    printf("Se recibió la señal SIGINT\n");
+}
+
+int main(int argc, const char * argv[]) {
+{
+    
+    struct sigaction senal;
+    
+    senal.sa_handler = gestor;
+    senal.sa_flags = SA_ONESHOT;
+    
+    if ( sigaction(SIGINT, &senal, 0) == -1)
+    {
+        printf("Error ");
+        exit(1);
+    }
+    
+    while(1);
+    
+    return 0;
+}
