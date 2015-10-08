0aeba21894d3b8daa1ebf3ee87920ca4dd39e38a
Vicente Cubells <vcubells@yahoo.com>
2015-10-08 15:29:15 -0500

    trabajar con conjunto de señales

---
+++ Tema_4_Señales/conjunto_señales/conjunto_señales/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  conjunto_señales
+//
+//  Created by Vicente Cubells Nonell on 08/10/15.
+//  Copyright © 2015 Vicente Cubells Nonell. All rights reserved.
+//
+
+#include <stdio.h>
+#include <signal.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    sigset_t ninguna, todas;
+    
+    sigemptyset(&ninguna);
+    sigfillset(&todas);
+    
+    sigaddset(&ninguna, SIGINT);
+    
+    
+    if (sigismember(&ninguna, SIGINT))
+        printf("SIGINT pertenece al conjunto ninguna\n");
+    else
+        printf("SIGINT no pertenece al conjunto ninguna \n");
+    
+    sigdelset(&todas, SIGINT);
+    
+    if (sigismember(&todas, SIGINT))
+        printf("SIGINT pertenece al conjunto todas \n");
+    else
+        printf("SIGINT no pertenece al conjunto todas \n");
+    
+    return 0;
+}
